5443057d524c0712734020d6ffd1b8b812a2a77e
Sara <saralkarsh@gmail.com>
2020-01-28 16:31:50 -0700

    Refactor pages to use layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Layout from '../components/layout'
 
 
 const About = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>About Me</h1>
       <p>I'm making this as part of a YouTube video on GatsbyJS. Pretend this is my personal site.</p>
       <p>You can find my contact information <Link to="/contact">here</Link></p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Layout from '../components/layout'
 
 const Contact = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <p>Email: saralkarsh@gmail.com</p>
       <p>Phone: you can't have it</p>
       <p>Go to <a href="https://www.twitter.com" target="_blank">Twitter</a></p>
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
