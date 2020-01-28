47f22491b9d3b1aee60e49368f10bd030d86b466
Sara <saralkarsh@gmail.com>
2020-01-28 15:12:38 -0700

    Add link using link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Link } from 'gatsby'
+
 
 const IndexPage = () => {
   return (
@@ -6,6 +8,8 @@ const IndexPage = () => {
       <h1>Hello!</h1>
       <h2>I'm Sara, and this is my repo where I'm going through a course on GatsbyJS</h2>
       <p><a href="/contact">Here's</a> a link to the contact page</p>
+      <p>Here's another way to link with the link component</p>
+      <Link to="/contact">Contact me.</Link>
     </div>
   )
 }
