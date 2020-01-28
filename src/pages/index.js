c6fb5e9eb7c472dfa79983c8659d680647024de8
Sara <saralkarsh@gmail.com>
2020-01-28 16:27:56 -0700

    Begin building out layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,18 @@ import { Link } from 'gatsby'
 
 import Footer from '../components/footer'
 import Header from '../components/header'
+import Layout from '../components/layout'
 
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
-      <h1>Hello!</h1>
-      <h2>I'm Sara, and this is my repo where I'm going through a course on GatsbyJS</h2>
-      <p><a href="/contact">Here's</a> a link to the contact page</p>
-      <p>Here's another way to link with the link component</p>
-      <Link to="/contact">Contact me.</Link>
-      <Footer />
-    </div>
+      <Layout>
+        <h1>Hello!</h1>
+        <h2>I'm Sara, and this is my repo where I'm going through a course on GatsbyJS</h2>
+        <p><a href="/contact">Here's</a> a link to the contact page</p>
+        <p>Here's another way to link with the link component</p>
+        <Link to="/contact">Contact me.</Link>
+      </Layout>
   )
 }
 
