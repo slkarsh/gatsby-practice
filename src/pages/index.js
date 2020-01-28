f29571daa3758ad99bd159612541bc55657c18a7
Sara <saralkarsh@gmail.com>
2020-01-28 15:09:01 -0700

    Make link to contact using anchor and href

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ const IndexPage = () => {
     <div>
       <h1>Hello!</h1>
       <h2>I'm Sara, and this is my repo where I'm going through a course on GatsbyJS</h2>
+      <p><a href="/contact">Here's</a> a link to the contact page</p>
     </div>
   )
 }
