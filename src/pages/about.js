76e64a3c9e7ae89b7196f67ba64b2c653e24d97c
Sara <saralkarsh@gmail.com>
2020-01-28 15:20:11 -0700

    Add internal link to contact page and external link to twitter

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const About = () => {
   return (
     <div>
       <h1>About Me</h1>
       <p>I'm making this as part of a YouTube video on GatsbyJS. Pretend this is my personal site.</p>
+      <p>You can find my contact information <Link to="/contact">here</Link></p>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ const Contact = () => {
     <div>
       <p>Email: saralkarsh@gmail.com</p>
       <p>Phone: you can't have it</p>
+      <p>Go to <a href="https://www.twitter.com" target="_blank">Twitter</a></p>
     </div>
   )
 }
