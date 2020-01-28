727ea5ee6b182149c87258d6ea8ce65dca4c69ac
Sara <saralkarsh@gmail.com>
2020-01-28 14:52:08 -0700

    Add about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I'm making this as part of a YouTube video on GatsbyJS. Pretend this is my personal site.</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <p>Email: saralkarsh@gmail.com</p>
+      <p>Phone: you can't have it</p>
+    </div>
+  )
+}
+
+export default Contact;
